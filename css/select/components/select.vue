79842a291a3ef0a7ea79170ff64c59c6e261ba0f
youtingkun <10816@ps.com>
2022-07-01 11:30:04 +0800

    css select

--- css/select/components/select.vue
+++ css/select/components/select.vue
@@ -1,47 +1,54 @@
 <template>
-	<div class="">
-		<h1>选择器</h1>
-		奇数行和偶数行
-		<div class="test-nth-child">
-			<div>div1</div>
-			<div>div2</div>
-			<div>div3</div>
-			<div>div4</div>
-			<div>div5</div>
-			<div>div6</div>
-			<div>div7</div>
-			<div>div8</div>
-			<div>div9</div>
-			<div>div10</div>
-		</div>
-		第一行
-		<div class="test-nth-child1">
-			<div>div1</div>
-			<div>div2</div>
-			<div>div3</div>
-			<div>div4</div>
-			<div>div5</div>
-			<div>div6</div>
-			<div>div7</div>
-			<div>div8</div>
-			<div>div9</div>
-			<div>div10</div>
-		</div>
-	</div>
+  <div>
+    <span>奇数行和偶数行:</span>
+    <pre>
+        <code >div:nth-child(2n + 1) </code>
+        <code >div:nth-child(2n) </code>
+      </pre>
+    <div class="test-nth-child">
+      <div>div1</div>
+      <div>div2</div>
+      <div>div3</div>
+      <div>div4</div>
+      <div>div5</div>
+      <div>div6</div>
+      <div>div7</div>
+      <div>div8</div>
+      <div>div9</div>
+      <div>div10</div>
+    </div>
+    <span>第一行和倒数偶数行:</span>
+    <pre>
+        <code >div:nth-child(1)  </code>
+        <code >div:nth-last-child(2n)</code>
+      </pre>
+
+    <div class="test-nth-child1">
+      <div>div1</div>
+      <div>div2</div>
+      <div>div3</div>
+      <div>div4</div>
+      <div>div5</div>
+      <div>div6</div>
+      <div>div7</div>
+      <div>div8</div>
+      <div>div9</div>
+      <div>div10</div>
+    </div>
+  </div>
 </template>
 
-<style lang='scss' scoped>
-.test-nth-child div:nth-child(2n+1){
-	background-color: #ff0000;
+<style lang="scss" scoped>
+.test-nth-child div:nth-child(2n + 1) {
+  background-color: #ff0000;
 }
-.test-nth-child div:nth-child(2n){
-	background-color: #00ff00;
+.test-nth-child div:nth-child(2n) {
+  background-color: #00ff00;
 }
-.test-nth-child1 div:nth-child(1){
-	background-color: #0000ff;
+.test-nth-child1 div:nth-child(1) {
+  background-color: #0000ff;
 }
-.test-nth-child1 div:nth-last-child(2n){
-	background-color: #0000ff;
+.test-nth-child1 div:nth-last-child(2n) {
+  background-color: #0000ff;
 }
-
 </style>
